62649c636ddb055bbeedd6693cd639ff9dc4bb85
riyakumari06 <riya.kumari6704@gmail.com>
2022-07-29 16:51:55 +0530

    sun comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
